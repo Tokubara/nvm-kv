f65df46dc7cebfc9cd96dcbba79e0db4177f02ef
Tokubara <1270802817@qq.com>
2021-05-08 21:18:28 +0800

    fix run.sh

--- bench/run.sh
+++ bench/run.sh
@@ -1,8 +1,9 @@
-rm result.csv
+rm -rf result.csv
 for threadNR in $(echo 1 $(seq 2 2 12)); do
   for readNR in {50,70,90,99}; do
     for isSkew in {0,1}; do
       ./bench $threadNR $readNR $isSkew >> result.csv
+      echo finish $threadNR $readNR $isSkew
       # echo "$threadNR $readNR $isSkew >> result.csv"
     done
   done
