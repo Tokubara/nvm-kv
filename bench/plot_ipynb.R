bfcb26f6ef7b7f5916f15c58197f0a33ba303d8e
Tokubara <1270802817@qq.com>
2021-05-09 10:10:54 +0800

    改plot, 不用子图

--- bench/plot_ipynb.R
+++ bench/plot_ipynb.R
@@ -14,16 +14,14 @@
 
 result_8<-read_csv("result_8.csv",col_names = c("threadNR","readNR","isSkewed","op"),col_types = list(col_integer(),col_integer(),col_factor(ordered=TRUE),col_double()))
 
-plot_8<-qplot(result_8, aes(x=threadNR, y=op, color=isSkewed)) +
+ggplot(result_8, aes(x=threadNR, y=op, color=isSkewed)) +
   geom_point() + 
-  facet_grid(. ~ readNR)
+  facet_grid(. ~ readNR)+ggtitle("BUCKET_NUM=8")
 
 result_32<-read_csv("result_32.csv",col_names = c("threadNR","readNR","isSkewed","op"),col_types = list(col_integer(),col_integer(),col_factor(ordered=TRUE),col_double()))
 
-plot_32<-qplot(result_32, aes(x=threadNR, y=op, color=isSkewed)) +
+ggplot(result_32, aes(x=threadNR, y=op, color=isSkewed)) +
   geom_point() + 
   facet_grid(. ~ readNR)+ggtitle("BUCKET_NUM=32")
-
-gridExtra::grid.arrange(plot_8,plot_32)
 
 result_32%>%dplyr::filter(isSkewed==1)%>%.$op
