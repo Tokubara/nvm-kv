02e26c0dab43cae42cdcda08f2dc668f7c8a78f4
Tokubara <1270802817@qq.com>
2021-05-09 09:48:12 +0800

    本来想把两幅图并成一幅图的, 用子图, 但是不行

--- bench/plot_ipynb.R
+++ bench/plot_ipynb.R
@@ -12,16 +12,18 @@
 #     name: ir
 # ---
 
-result<-read_csv("result.csv",col_names = c("threadNR","readNR","isSkewed","op"),col_types = list(col_integer(),col_integer(),col_factor(ordered=TRUE),col_double()))
+result_8<-read_csv("result_8.csv",col_names = c("threadNR","readNR","isSkewed","op"),col_types = list(col_integer(),col_integer(),col_factor(ordered=TRUE),col_double()))
 
-ggplot(result, aes(x=threadNR, y=op, color=isSkewed)) +
+plot_8<-qplot(result_8, aes(x=threadNR, y=op, color=isSkewed)) +
   geom_point() + 
   facet_grid(. ~ readNR)
 
 result_32<-read_csv("result_32.csv",col_names = c("threadNR","readNR","isSkewed","op"),col_types = list(col_integer(),col_integer(),col_factor(ordered=TRUE),col_double()))
 
-ggplot(result_32, aes(x=threadNR, y=op, color=isSkewed)) +
+plot_32<-qplot(result_32, aes(x=threadNR, y=op, color=isSkewed)) +
   geom_point() + 
-  facet_grid(. ~ readNR)
+  facet_grid(. ~ readNR)+ggtitle("BUCKET_NUM=32")
+
+gridExtra::grid.arrange(plot_8,plot_32)
 
 result_32%>%dplyr::filter(isSkewed==1)%>%.$op
